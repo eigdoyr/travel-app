3199029b1c6158029dc793486853b01db72a3310
ryodgie <ryodgie@gmail.com>
2023-04-11 23:35:07 +0800

    feat: add views and router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/Brazil">Brazil</router-link>
+    <router-link to="/Hawaii">Hawaii</router-link>
+    <router-link to="/Jamaica">Jamaica</router-link>
+    <router-link to="/Panama">Panama</router-link>
+  </div>
+
+  <div class="container">
+    <router-view></router-view>
+  </div>
+</template>
 
 <style scoped></style>
