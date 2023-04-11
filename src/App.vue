215500f7b95e79f7a641ce44099c8b18abdf7726
ryodgie <ryodgie@gmail.com>
2023-04-11 23:35:07 +0800

    feat: add views and router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/About">About</router-link>
+  <router-view></router-view>
+</template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 import "./style.css";
 import App from "./App.vue";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [],
-});
+import router from "@/router";
 
 createApp(App).use(router).mount("#app");
