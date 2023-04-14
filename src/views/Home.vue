78330e8814c07ab253dfe42eb7215fdaeb8f4c2b
ryodgie <ryodgie@gmail.com>
2023-04-14 19:33:08 +0800

    refactor: static to dynamic

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <router-link
+        v-for="{ id, name, image } in destinations"
+        :key="id"
+        :to="{ name: 'destination.show', params: { id: id } }"
+      >
+        <h2>{{ name }}</h2>
+        <img :src="`/images/${image}`" :alt="name" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import sourceData from "@/data.json";
+
+const destinations = sourceData.destinations;
+</script>
